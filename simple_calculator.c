1bbebacdf5fffac332a7d2d667414ec1ba45c8c1
adyashapadhy25 <adyashapadhy05@gmail.com>
2025-02-14 20:59:38 +0530

    Add files via upload
    c program using switch-case statements to perform basic arithmetic operations(add, subtract, multiply, divide) based on user input.

---
+++ simple_calculator.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+ 
+int main()
+{
+	char Operator;
+	float num1, num2, result = 0;
+	
+	printf("\n Enter an Operator (+, -, *, /)  :  ");
+  	scanf("%c", &Operator);
+  	
+	printf("\n Enter the Values for two Operands: num1 and num2  :  ");
+  	scanf("%f%f", &num1, &num2);
+  	
+  	switch(Operator)
+  	{
+  		case '+':
+  			result = num1 + num2;
+  			break;
+  		case '-':
+  			result = num1 - num2;
+  			break;  			
+  		case '*':
+  			result = num1 * num2;
+  			break;
+  		case '/':
+  			result = num1 / num2;
+  			break;
+  			default:
+  				printf("\n invalid operator");
+  			}
+  			printf("\n the result of %.2f %c %.2f=%.2f",num1,Operator,num2,result);
+  			return 0;
+  		}
